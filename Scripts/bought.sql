a670c86a8f99637754aaf74ea133e8770cd65f19
Dillen E Roggensinger <der2127@bucharest.clic.cs.columbia.edu>
2011-04-03 18:57:01 -0400

    Scripts complete

--- Scripts/bought.sql
+++ Scripts/bought.sql
@@ -10,14 +10,14 @@ foreign key (username) references account(username)
 alter table bought
 add primary key(did,username);
 
-INSERT INTO bought VALUES('ninjaturtles',3);
-INSERT INTO bought VALUES('sexytime',4);
-INSERT INTO bought VALUES('sexytime',8);
-INSERT INTO bought VALUES('ragingbull',10);
-INSERT INTO bought VALUES('ragingbull',12);
-INSERT INTO bought VALUES('cookiemonster',19);
-INSERT INTO bought VALUES('friday',20);
-INSERT INTO bought VALUES('mysticshadow',25);
-INSERT INTO bought VALUES('cookiemonster',35);
-INSERT INTO bought VALUES('bigdaddy',39);
-INSERT INTO bought VALUES('trollolol',41);
+INSERT INTO bought VALUES('ninjaturtles',3,1,to_date('2000/01/23:12:00:00AM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('sexytime',4,2,to_date('2010/02/21:12:00:00AM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('sexytime',8,1,to_date('2009/01/23:10:00:00PM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('ragingbull',10,3,to_date('2011/03/18:11:32:00AM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('ragingbull',12,2,to_date('2009/02/11:02:11:20AM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('cookiemonster',19,1,to_date('2008/08/02:03:23:12PM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('friday',20,5,to_date('2008/09/22:02:53:23PM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('mysticshadow',25,6,to_date('2010/08/12:03:43:12AM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('cookiemonster',35,1,to_date('2009/06/22:04:13:12PM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('bigdaddy',39,1,to_date('2011/02/21:12:54:16PM', 'yyyy/mm/dd:hh:mi:ssam'));
+INSERT INTO bought VALUES('trollolol',41,1,to_date('2009/03/01:01:12:32PM', 'yyyy/mm/dd:hh:mi:ssam'));
--- Scripts/favorite.sql
+++ Scripts/favorite.sql
@@ -1,21 +1,21 @@
 create table favorite(
-aid integer,
+username varchar(20),
 did integer,
 foreign key (did) references alcohol(did),
-foreign key (aid) references account(aid)
+foreign key (username) references account(username)
 );
 
 alter table favorite
-add primary key(did,aid);
+add primary key(did,username);
 
-INSERT INTO favorite VALUES(6,1);
-INSERT INTO favorite VALUES(3,5);
-INSERT INTO favorite VALUES(1,6);
-INSERT INTO favorite VALUES(5,6);
-INSERT INTO favorite VALUES(5,7);
-INSERT INTO favorite VALUES(7,15);
-INSERT INTO favorite VALUES(9,18);
-INSERT INTO favorite VALUES(4,23);
-INSERT INTO favorite VALUES(10,26);
-INSERT INTO favorite VALUES(7,31);
-INSERT INTO favorite VALUES(8,39);
+INSERT INTO favorite VALUES('trollolol',1);
+INSERT INTO favorite VALUES('bigdaddy',5);
+INSERT INTO favorite VALUES('newuser',6);
+INSERT INTO favorite VALUES('friday',6);
+INSERT INTO favorite VALUES('ragingbull',7);
+INSERT INTO favorite VALUES('bigdaddy',15);
+INSERT INTO favorite VALUES('friday',18);
+INSERT INTO favorite VALUES('trollolol',23);
+INSERT INTO favorite VALUES('newuser',26);
+INSERT INTO favorite VALUES('friday',31);
+INSERT INTO favorite VALUES('ragingbull',39);
