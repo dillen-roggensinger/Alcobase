17fd2688ff874d09a980db34741fb154cb678ef2
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-04-03 19:27:48 -0400

    weee!

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
-<!DOCTYPE form PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
+<head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><title>Alcobase</title><link href="Style.css" rel="stylesheet" type="text/css"><style type="text/css"><!--.style1 {font-family: Arial, Helvetica, sans-serif}--></style>
+</head><body>
+<div id="header">
+<a href="index.html"><h1>ALCOBASE</h1></a>
+</div>
+<div id="contents">
+<form action="cgi-bin/login.pl.cgi" method="POST">
+<table align="center">
+	<tr>
+		<td><a href="login.php"><h2>Welcome to Alcobase, your one stop source for all your alcoholic--I meancollege student--needs!</h2></a></td>
+	</tr>
+	<tr>
+		<td><a href="login.php"><h2>Login</h2></a></td>
+	</tr>
+	<tr>
+		<td><a href="browse.php"><h2>Browse</h2></a></td>
+	</tr>
+	<tr>
+		<td><a href="signup.php"><h2>Sign Up</h2></a></td>
+	</tr>
+</table>
+<p align="center"><input type="submit" value="Log In"></p>
 
-</head>
-<body>
-
-
-
-
-<p>I ain't a part of your system!</p>
-
+</form>
+</div>
 </body>
 </html>
-<<<<<<< HEAD
-
-PLEASE DON'T CHANGE ME :(
