b8eaead53efa0f310b590763e0301df5d447a662
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-04-03 18:47:57 -0400

    weee!

--- index.php
+++ index.php
@@ -6,32 +6,7 @@
 <body>
 
 
-<?php
 
-//include("webpage.php");
-
-$con = oci_connect("der2127", "c00kie5", "w4111c.cs.columbia.edu:1521/adb");
-if (!$con){
-	$e = oci_error();
-	echo "<br />ERROR: ".$e['message']."<br />";
-}
-
-$query = "SELECT * FROM alcohol, sold_at where alcohol.did=sold_at.did";
-
-
-
-$stid = oci_parse($con, $query);
-$err=oci_execute($stid);
-
-
-while($row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS)){
-echo $row['NAME'] . "  " . $row['LOCATION'] . "  " . $row['PRICE'];
-echo "<br />";
-}
-
-
-oci_close($con);
-?>
 
 <p>I ain't a part of your system!</p>
 
