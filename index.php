7fdbfe48ccda80588f2443b021e4f0b7c16c0a0e
Dillen E Roggensinger <der2127@bucharest.clic.cs.columbia.edu>
2011-04-03 10:27:42 -0400

    Setting up file system

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE form PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+</head>
+<body>
+
+
+<?php
+
+//include("webpage.php");
+
+$con = oci_connect("der2127", "c00kie5", "w4111c.cs.columbia.edu:1521/adb");
+if (!$con){
+	$e = oci_error();
+	echo "<br />ERROR: ".$e['message']."<br />";
+}
+
+$query = "SELECT * FROM alcohol, sold_at where alcohol.did=sold_at.did";
+
+
+
+$stid = oci_parse($con, $query);
+$err=oci_execute($stid);
+
+
+while($row = oci_fetch_array($stid,OCI_BOTH+OCI_RETURN_NULLS)){
+echo $row['NAME'] . "  " . $row['LOCATION'] . "  " . $row['PRICE'];
+echo "<br />";
+}
+
+
+oci_close($con);
+?>
+
+
+
+</body>
+</html>
