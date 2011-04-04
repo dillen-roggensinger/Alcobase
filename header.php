8f71a628b8eca009176cdde8a326d0f51667dacd
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-04-03 20:37:28 -0400

    did some more stuff

--- header.php
+++ header.php
@@ -1,8 +1,6 @@
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><title>Stunt</title><link href="Style.css" rel="stylesheet" type="text/css"><style type="text/css"><!--.style1 {font-family: Arial, Helvetica, sans-serif}--></style>
+<head><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><title>Alcobase</title><link href="Style.css" rel="stylesheet" type="text/css"><style type="text/css"><!--.style1 {font-family: Arial, Helvetica, sans-serif}--></style>
 </head><body>
 <div id="header">
-<h1>STUNT</h1><h3><a href="index.html">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="signup.html">Sign Up</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="password.html">Reset Password</a></h3>
+<h1>ALCOBASE</h1>
 </div>
