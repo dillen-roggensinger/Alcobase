a4346ee740b6f07e64e460de69d4e97fe5ebd0f7
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-04-03 21:35:32 -0400

    did some more stuff

--- headerfooter.php
+++ headerfooter.php
@@ -4,10 +4,10 @@ class HeaderFooter{
 		echo "<html>\n
 			<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"><title>Alcobase</title><link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"><style type=\"text/css\"><!--.style1 {font-family: Arial, Helvetica, sans-serif}--></style>\n
 			</head><body>\n
-			<div id=\"header\">\n
+			<div id='header'>\n
 			<h1>ALCOBASE</h1>\n
 			</div>\n
-			<div>";
+			<div id='content'>";
 	}
 	public function footer(){
 		echo "</div>\n
