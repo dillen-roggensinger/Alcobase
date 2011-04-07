08384a27ea8cd931e4e755fcb0180e9666b068a1
Stephanie Aligbe <sna2111.cs@gmail.com>
2011-04-07 05:39:16 -0400

    secure

---
+++ ds_db_index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
+<?php
+include('headerfooter.php'); 
+$hf = new HeaderFooter();
+$hf->header();
+?>
+
+<table align="center">
+	<tr>
+		<td><h3>Welcome to Alcobase, the one stop source for all your college essentials!</h3></td>
+	</tr>
+</table>
+
+<?php 
+$hf->footer();
+?>
